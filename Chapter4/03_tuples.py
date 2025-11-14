4439d4d06a958f70b1331efbc1887f1fd8535452
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-11-14 23:07:41 +0530

    tuples

---
+++ Chapter4/03_tuples.py
@@ -0,0 +1,11 @@
+a = (1, 2, 5, 6)
+print(type(a))  
+b = ()
+print(type(b))
+
+c = (1)
+print(type(c)) # Not a tuple, just an integer
+d = (1,)
+print(type(d)) # This is a tuple
+e = (1,45,342,3424,False, "Rohan", "Shivam")
+print(type(e))
