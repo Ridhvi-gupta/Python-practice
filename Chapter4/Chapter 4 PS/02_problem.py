776e8d4966d226f3836735d2edab416cd7a3b288
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-11-19 00:53:40 +0530

    problem

---
+++ Chapter4/Chapter 4 PS/02_problem.py
@@ -0,0 +1,16 @@
+marks = []
+
+f1 = input("Enter Marks here: ")
+marks.append(f1)   
+f2 = input("Enter Marks here: ")
+marks.append(f2) 
+f3 = input("Enter Marks here: ")
+marks.append(f3)
+f4 = input("Enter Marks here: ")
+marks.append(f4)
+f5 = input("Enter Marks here: ")
+marks.append(f5)
+f6 = input("Enter Marks here: ")
+marks.append(f6)
+
+print(marks)
