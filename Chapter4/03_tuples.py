1fda9cbc2e24ac75321d04325ea287551df047a3
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-11-29 02:09:58 +0530

    tuples

--- Chapter4/03_tuples.py
+++ Chapter4/03_tuples.py
@@ -1,7 +1,15 @@
 a = (1, 2, 5, 6)
 print(type(a))  
+a = (1)
+print(type(a))
+a1 = (1,)  # Tuple with single element
+print(type(a1))
 b = ()
 print(type(b))
+a = (1,45,342,3424,False, "Rohan", "Shivam")
+# a[0] = 4532  # This will raise an error since tuples are immutable
+print(a)
+print(type(a))
 
 c = (1)
 print(type(c)) # Not a tuple, just an integer
