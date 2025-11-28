1944d43eb406ca140075ebbf22dfe9a07311721b
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-11-28 21:19:37 +0530

    list methods

--- Chapter4/02_list_methods.py
+++ Chapter4/02_list_methods.py
@@ -6,9 +6,12 @@ friends.append("Harry")
 print(friends)
 
 l1 = [1, 34, 62, 2,6, 11]
-#l1.sort()
+# l1.sort()
 # l1.reverse()
-# l1.insert(2, 333333) # Insert 333333 dsuch that its index in the list is 2
-print(l1.pop(3))
+# l1.insert(2, 333333) # Insert 333333 such that its index in the list is 2
+value =l1.pop(3)
+print(value)
+# l1.pop(3)  # Removes element at index 3
+# print(l1.pop(3))
 print(l1)
 
