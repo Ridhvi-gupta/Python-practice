cdac9ef9590bd5431db608cb49421be6f00b504f
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-11-30 02:33:44 +0530

    dictionary methods

---
+++ Chapter5/02_dict_methods.py
@@ -0,0 +1,21 @@
+marks1 = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Harry"
+}
+# print(marks1.items())  # prints all key-value pairs as tuples in a list
+# print(marks1.keys())    # prints all the keys in the dictionary
+# print(marks1.values())  # prints all the values in the dictionary
+
+# marks1.update({"Harry": 99})  # updates the value for the key "Harry"
+# marks1.update({"Harry": 99, "Renuka": 100})  # updates "Harry" and adds a new key "Renuka"
+# print(marks1)  # prints the updated dictionary
+
+# print(marks1.get("Harry")) 
+# print(marks1["Harry"]) 
+
+print(marks1.get("Harry2")) # Prints None
+print(marks1["Harry2"]) # Returns an error
+
+
