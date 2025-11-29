84f1d98a57a0ee437b04fe9795a66fdd10ea4c29
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-11-30 02:56:57 +0530

    sets

--- Chapter5/03_sets.py
+++ Chapter5/03_sets.py
@@ -1,2 +1,7 @@
-s = {} # Empty dictionary
-s = {1, 5, 32} # Set
+# s = {} # Empty dictionary
+# s = {1, 5, 32} # Set
+# e = set() # Don't use s = {} as it will create an empty dictionary
+# print(type(s))
+
+s1 = {1, 5, 32, 54, 5, 5, 5 }
+print(s1)
