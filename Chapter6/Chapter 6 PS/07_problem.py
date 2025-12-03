84b20d62d65769e33b89e112288f45bcccb3ee0a
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-12-03 19:52:13 +0530

    problem

---
+++ Chapter6/Chapter 6 PS/07_problem.py
@@ -0,0 +1,18 @@
+# post = "Hey harry bhai is good Harry is very good and harry is great"
+
+# if("Harry" in post):
+#     print("This post is talking about harry")
+
+# post1 = input("Enter the post1: ")
+# if("Harry" in post1):
+#     print("This post1 is talking about harry")
+# else:
+#     print("This post1 is not talking about Harry")
+
+post2 = input("Enter the post2: ")
+if("Harry".lower() in post2.lower()):
+    print("This post2 is talking about harry")
+else:
+    print("This post2 is not talking about harry")
+
+
