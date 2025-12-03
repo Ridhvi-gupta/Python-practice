ab33a609a6ac48c66689394f978cc7c9e12b6e66
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-12-03 18:11:51 +0530

    problem

---
+++ Chapter6/Chapter 6 PS/03_problem.py
@@ -0,0 +1,14 @@
+# Program to filter spam 
+p1 = "Make a lot of money"
+p2 = "buy now"
+p3 = "subscribe now"
+p4 = "click this"
+
+message = input("Enter your comment: ")
+
+if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+    print("This comment is a spam")
+
+else:
+    print("This comment is not a spam")
+   
