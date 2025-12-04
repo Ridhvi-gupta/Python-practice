03b08f69b9927b966cba443ca7ef26f66af12146
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-12-04 09:49:00 +0530

    problem

---
+++ Chapter7/while_loops.py
@@ -0,0 +1,40 @@
+# i = 1
+
+# while(i<6):
+#     print(i)
+#     i += 1 i = i + 1
+
+#     '''
+#     Output:
+#     1
+#     2
+#     3
+#     4
+#     5
+#     '''
+
+# Program to print 1 to 50 
+# i = 1
+
+# while(i<51):
+#         print(i)
+#         i += 1
+
+# Program to print Harry 5 times
+i = 0
+while(i<5): # print "Harry" - 5 times!
+        print("Harry")
+        i = i + 1
+ # OR
+i1 = 1
+while(i1<6): 
+        print("Harry")
+        i1 = i1 + 1
+# OR
+i2 = 1
+while i2 <= 5:
+        print("Harry")
+        i2 += 1
+
+# All these 3 programs give same output wrritten using # OR
+
