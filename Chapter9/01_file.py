7877114d4da4baccd68e2b0a4650d789609b1537
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-12-06 16:15:02 +0530

    for loops iteration

---
+++ Chapter9/01_file.py
@@ -0,0 +1,16 @@
+'''
+
+a = "a very long string with emails"
+
+emails = []
+3 seconds  # Time taken to run the program
+
+'''
+
+# To read a file in Python
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
+
+
