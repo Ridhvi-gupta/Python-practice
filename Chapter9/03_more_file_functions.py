f3a0913ed4c2c717c5dbcbf55486942dc570d83f
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-12-09 20:09:34 +0530

    more file functions

---
+++ Chapter9/03_more_file_functions.py
@@ -0,0 +1,24 @@
+f = open("file.txt")
+
+# lines = f.readlines() # Reads all lines into a list
+# print(lines, type(lines))
+
+# line1 = f.readline()  # Reads a single line from the file
+# print(line1, type(line1))
+# line2 = f.readline()  
+# print(line2, type(line2))
+# line3 = f.readline()  
+# print(line3, type(line3))
+# line4 = f.readline()  
+# print(line4, type(line4))
+# line5 = f.readline()  # no more lines to read, so no output
+# # print(line5, type(line5))
+# print(line5 =="") # True, empty string
+
+##  This can be done with the help of a loop, as above code is very cumbersome process  
+line = f.readline()
+while f.readline():
+    print(line)
+
+
+f.close()
