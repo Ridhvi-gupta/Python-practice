1c5717f0523630caea82718664a2d8cdf8c58f73
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-12-20 21:31:18 +0530

    class and objects

---
+++ Chapter10/class.py
@@ -0,0 +1,10 @@
+# Creating class
+class Employee:
+    name = "Harry"
+    language = "Py"
+    salary = 1200000
+
+# Creating object of class, harry is a object of class Employee
+harry = Employee() # Employee() is a memory allocation statement
+# Accessing class attributes using object
+print(harry.name, harry.language)
