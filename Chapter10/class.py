0743a6ee845a1259983c462cc761dd40ebd0f420
Ridhvi Gupta <ridhvigupta3@gmail.com>
2025-12-21 20:27:12 +0530

    class and object

--- Chapter10/class.py
+++ Chapter10/class.py
@@ -1,10 +1,21 @@
 # Creating class
 class Employee:
-    name = "Harry"
-    language = "Py"
+    # name = "Harry"  If name is not given in class attributes
+    language = "Py" # This is a class attribute
     salary = 1200000
 
 # Creating object of class, harry is a object of class Employee
+# harry is first object of class Employee
 harry = Employee() # Employee() is a memory allocation statement
+harry.name = "Harry" # This is an object attribute
 # Accessing class attributes using object
-print(harry.name, harry.language)
+# print(harry.name)
+print(harry.name, harry.language, harry.salary)
+
+# Creating another object of class Employee
+rohan = Employee()
+rohan.name = "Rohan Roro Robinson" # Dynamically adding attribute to object
+print(rohan.name, rohan.salary, rohan.language)
+
+# Here, name is an instance attribute and language, salary are class attributes as they directly belong to class
+# Oject attributes are also known as instance attributes
