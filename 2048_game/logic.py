4a65cfcc0e643070c9d4e39a4f6f9cb311d0539e
Ridhvi Gupta <ridhvigupta3@gmail.com>
2026-09-01 09:41:54 +0530

    logic

--- 2048_game/logic.py
+++ 2048_game/logic.py
@@ -1,1 +1,10 @@
 # Implements the game logic, including tile movement, merging, random tile generation, and game state checks.
+
+import random
+
+def start_game():
+    mat =[]
+    for i in range(4):
+        mat.append([0] * 4)
+
+    print("Commands are as follows : ")
