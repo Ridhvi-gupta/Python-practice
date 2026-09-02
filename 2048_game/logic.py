15bcfa85b1f54d82f0aea2192377a091e1e2bb78
Ridhvi Gupta <ridhvigupta3@gmail.com>
2026-09-02 16:23:03 +0530

    logic

--- 2048_game/logic.py
+++ 2048_game/logic.py
@@ -8,3 +8,10 @@ def start_game():
         mat.append([0] * 4)
 
     print("Commands are as follows : ")
+    print("'W' or 'w' : Move Up")
+    print("'S' or 's' : Move Down")
+    print("'A' or 'a' : Move Left")
+    print("'D' or 'd' : Move Right")
+
+    add_new_2(mat)
+    return mat
