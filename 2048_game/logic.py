00639c42c8ccbd66bda32017ac35fb34ed109376
Ridhvi Gupta <ridhvigupta3@gmail.com>
2026-09-15 07:55:06 +0530

    logic

--- 2048_game/logic.py
+++ 2048_game/logic.py
@@ -63,7 +63,6 @@ def compress(mat):
     new_mat = []
     for i in range(4):
         new_mat.append([0] * 4)
-        for i in range(4):
         pos = 0
         for j in range(4):
             if(mat[i][j] != 0):
