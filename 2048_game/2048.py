3099404bacd0dd7c98656c890ac4aadbe64ec673
Ridhvi Gupta <ridhvigupta3@gmail.com>
2026-09-25 22:56:38 +0530

    2048

--- 2048_game/2048.py
+++ 2048_game/2048.py
@@ -4,3 +4,10 @@ import logic
 
 if __name__ == '__main__':
         mat = logic.start_game()
+
+while(True):
+    x = input("Press the command : ")
+    if(x == 'W' or x == 'w'):
+        mat, flag = logic.move_up(mat)
+        status = logic.get_current_state(mat)
+        print(status)
